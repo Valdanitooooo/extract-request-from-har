c90192873c0c2169977b0c5d991f523823c2c0ff
valdanito <valdanito@foxmail.com>
2022-01-17 19:11:47 +0800

    feat: add jmx arg & build a fat jar

---
+++ README.md
@@ -0,0 +1,16 @@
+# 从HAR（HTTP存档格式)文件中提取URL
+
+## usage
+
+```
+mvn clean compile assembly:single
+
+java -jar target/extract-urls-from-har-1.0-SNAPSHOT.jar -h
+
+usage: help info
+ -h,--help        The command help
+ -i,--in <arg>    Location of HAR file.
+ -j,--jmx         Output JMX format.
+ -o,--out <arg>   Location of urls file.
+
+```
--- src/main/java/cn/valdanito/Main.java
+++ src/main/java/cn/valdanito/Main.java
@@ -1,3 +1,5 @@
+package cn.valdanito;
+
 import com.jayway.jsonpath.JsonPath;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -22,13 +24,34 @@ public class Main {
         options.addOption("h", "help", false, "The command help");
         options.addOption("i", "in", true, "Location of HAR file.");
         options.addOption("o", "out", true, "Location of urls file.");
-
+        options.addOption("j", "jmx", false, "Output JMX format.");
     }
 
-    public static void help() {
+    private static void help() {
         HelpFormatter hf = new HelpFormatter();
         hf.printHelp("help info", options);
     }
+
+    private static String format(List<String> flows) {
+        StringBuilder urls = new StringBuilder();
+        for (String flow : flows) {
+            flow = flow.replaceAll("&", "&amp;");
+            urls.append(flow);
+            urls.append("\n");
+        }
+        return urls.toString();
+    }
+
+    private static String formatJMX(List<String> flows) {
+        StringBuilder urls = new StringBuilder();
+        for (String flow : flows) {
+            flow = flow.replaceAll("&", "&amp;");
+            urls.append("<collectionProp name=\"1357392467\">\n   <stringProp name=\"726316312\">"
+                    + flow + "</stringProp>\n</collectionProp>\n");
+        }
+        return urls.toString();
+    }
+
 
     public static void main(String[] args) {
         if (null == args || args.length == 0) {
@@ -54,21 +77,26 @@ public class Main {
             }
             String input_path = cmd.getOptionValue("i");
             String output_path = cmd.getOptionValue("o");
+            String jmx = cmd.getOptionValue("j");
+
             FileInputStream fis = new FileInputStream(input_path);
             final String str = IOUtils.toString(fis, "UTF-8");
             List<String> flows = JsonPath.parse(str).read("$.log.entries[*].request.url");
+            if (flows.isEmpty()) {
+                System.out.println("No URLs");
+            }
             String urls = "";
-            for (String flow : flows) {
-                flow = flow.replaceAll("&", "&amp;");
-                urls += "<collectionProp name=\"1357392467\">\n   <stringProp name=\"726316312\">"
-                        + flow + "</stringProp>\n</collectionProp>\n";
+            if (cmd.hasOption("j")) {
+                urls = formatJMX(flows);
+            } else {
+                urls = format(flows);
             }
             File file = new File(output_path);
             FileOutputStream outputStream = new FileOutputStream(file);
             InputStream is = new ByteArrayInputStream(
                     urls.getBytes("utf8"));
             IOUtils.copy(is, outputStream);
-            System.out.println("success");
+            System.out.println("Success");
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
